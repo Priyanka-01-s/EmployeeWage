52506657bf837ec6615f4da753f6f4cb25233a48
Priyanka Sengupta <78321696+Priyanka-01-s@users.noreply.github.com>
2023-11-21 17:03:21 +0530

    Employee daily wage use case

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,6 +1,17 @@
 package Assgn2;
+
 public class EmployeeWage {
-    public static void main(String[] args){
+
+    private static double calculateWage(int hoursWorked, double wagePerHour) {
+        double dailyWage = hoursWorked * wagePerHour;
+        return dailyWage;
+    }
+    public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+
+        int hoursWorked = 8;
+        double wagePerHour = 20.0;
+        double dailyWage = calculateWage(hoursWorked, wagePerHour);
+        System.out.println("Daily Employee Wage: $" + dailyWage);
     }
 }
