6e5f0c35992ea1bced74de43c59fd5504d2f9c65
Priyanka Sengupta <78321696+Priyanka-01-s@users.noreply.github.com>
2023-11-21 17:36:41 +0530

    Employee switch case use case

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,6 +1,36 @@
 package Assgn2;
 public class EmployeeWage {
+
+    private static double calculateDailyWage(int hoursWorked, double wagePerHour) {
+        
+        final int fullDayHours = 8;
+        final int partTimeHours = 4;
+        double dailyWage;
+
+        switch (hoursWorked) {
+            case fullDayHours:
+                dailyWage = hoursWorked * wagePerHour;
+                break;
+            case partTimeHours:
+                dailyWage = hoursWorked * wagePerHour;
+                break;
+            default:
+                System.out.println("Invalid hours worked.");
+                dailyWage = 0.0;
+        }
+
+        return dailyWage;
+    }
     public static void main(String[] args){
         System.out.println("Welcome to Employee Wage Computation Program");
+        double wagePerHour = 20.0;
+
+        int fullTimeHours = 8;
+        double fullTimeWage = calculateDailyWage(fullTimeHours, wagePerHour);
+        System.out.println("Full Time Employee Daily Wage: $" + fullTimeWage);
+
+        int partTimeHours = 4;
+        double partTimeWage = calculateDailyWage(partTimeHours, wagePerHour);
+        System.out.println("Part Time Employee Daily Wage: $" + partTimeWage);
     }
 }
