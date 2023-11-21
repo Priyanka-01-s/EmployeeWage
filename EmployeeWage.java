a749f8e6703517ffcc4d40e1d20d8f0f64aeb356
Priyanka Sengupta <78321696+Priyanka-01-s@users.noreply.github.com>
2023-11-21 16:54:48 +0530

    Employee atteandance use case

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,6 +1,19 @@
 package Assgn2;
 public class EmployeeWage {
+
+    private static void checkAttendance() {
+        
+        double attendanceValue = Math.random();
+
+        if (attendanceValue > 0.5) {
+            System.out.println("Employee is present.");
+        } else {
+            System.out.println("Employee is absent.");
+        }
+    }
     public static void main(String[] args){
-        System.out.println("Welcome to Employee Wage Computation Program");
+        System.out.println("Welcome to Employee Wage Computation Program\n");
+        System.out.println("ATTANDANCE");
+        checkAttendance();
     }
 }
