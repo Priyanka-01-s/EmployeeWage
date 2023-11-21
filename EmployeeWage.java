1b18cf2cc5c4741aa26b3109fd63e022b0a0509a
Priyanka Sengupta <78321696+Priyanka-01-s@users.noreply.github.com>
2023-11-21 17:50:10 +0530

    Employee Monthly salary use case

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,6 +1,26 @@
 package Assgn2;
+
+import java.util.Scanner;
+
 public class EmployeeWage {
+
+    public static double monthlyAmount(int workingdays){
+        int hours = 8;
+        double wage = 20.0;
+        double amount = hours*wage*workingdays;
+
+        return amount;
+    }
     public static void main(String[] args){
         System.out.println("Welcome to Employee Wage Computation Program");
+        System.out.println("Enter the number of working days in a month");
+        Scanner sc = new Scanner(System.in);
+        int workingdays = sc.nextInt();
+        double totalAmt;
+        totalAmt = monthlyAmount(workingdays);
+        System.out.print("The monthly salary of the employee(only full working days): "+ totalAmt);
+
+        sc.close();
+
     }
 }
