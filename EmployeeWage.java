b5b370965ee9cca27fdd134055aef8c850262a12
Priyanka Sengupta <78321696+Priyanka-01-s@users.noreply.github.com>
2023-11-21 17:30:06 +0530

    Employee part time wage use case

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,6 +1,35 @@
 package Assgn2;
+
+import java.util.Scanner;
+
 public class EmployeeWage {
-    public static void main(String[] args){
+    private static double calculatePartTime(int hoursWorked, double wagePerHour) {
+
+        int fullDayHours = 8;
+        int partTimeHours = 4;
+        double dailyWage = 0.0;
+        dailyWage = hoursWorked * wagePerHour;
+        if (hoursWorked == fullDayHours || hoursWorked == partTimeHours) {
+            dailyWage = hoursWorked * wagePerHour;
+        } else {
+            System.out.println("Invalid hours worked.");
+        }
+
+        return dailyWage;
+    }
+
+    public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+        System.out.println("Enter the working hours of the employeee");
+        Scanner sc = new Scanner(System.in);
+        int hours = sc.nextInt();
+        double wagePerHour = 20.0;
+        double partTimeWage = calculatePartTime(hours, wagePerHour);
+        System.out.println("Employee Wage: $" + partTimeWage);
+
+       
+
+        sc.close();
+
     }
 }
