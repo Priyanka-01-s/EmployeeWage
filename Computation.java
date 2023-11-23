c7b15997abaf924864f2b0d33be11ba20393f461
Priyanka Sengupta <78321696+Priyanka-01-s@users.noreply.github.com>
2023-11-23 11:20:03 +0530

    SwitchCase

--- Computation.java
+++
@@ -1,7 +0,0 @@
-package Assgn2;
-
-public class Computation {
-    public static void main(String[] args){
-        System.out.println("Welcome to Line Comparison Computation Program");
-    }
-}
--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,6 +1,13 @@
+//package Assgn2;
+
 package Assgn2;
+
 public class EmployeeWage {
-    public static void main(String[] args){
+    private static double calculateDailyWage(int hoursWorked, double wagePerHour) {
+        return hoursWorked * wagePerHour;
+    }
+
+    public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
     }
 }
